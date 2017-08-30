41857bd37670e1763478cc00b3dadb983a4ad7fe
Bryan <brbriv@gmail.com>
2017-08-30 13:13:18 -0400

    First commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var http = require('http'),
 var listingData, server;
 
 var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
+  var pathname = url.parse(request.url).pathname;
 
   /*
     Your request handler should send listingData in the JSON format if a GET request 
@@ -16,6 +16,17 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+    if (pathname == '/listings') {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(listingData);
+        response.end();
+    }
+    else{
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write('Bad gateway error');
+        response.end();
+    }
+    
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
@@ -23,4 +34,12 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+    listingData = data;
+    
+    if (err) {
+        return console.log(err)
+    }
+    http.createServer(requestHandler).listen(8080);
+    console.log('Server started');
 });
+
